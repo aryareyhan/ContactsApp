36b475e5af3654651ff518bf5cc4941f0576beb6
lippybae <aryareyhann@gmail.com>
2023-10-13 11:26:12 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ContactApp from "./components/ContactApp";
+
+// styling
+import "./styles/style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<ContactApp />);
---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Kim Jungeun',
+        tag: 'kimxxlip',
+        imageUrl: '/images/lippie.jpeg',
+      },
+      {
+        id: 2,
+        name: 'Son Hyeju',
+        tag: 'lnxexu',
+        imageUrl: '/images/hyeju.jpg',
+      },
+      {
+        id: 3,
+        name: 'Kim Hyunjin',
+        tag: 'hyunjinab',
+        imageUrl: '/images/hyunjin.jpg',
+      },
+    ];
+   }
+    
+   export { getData };
